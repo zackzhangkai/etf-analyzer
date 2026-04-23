ed173f2661a4c254dea61432d166fbd7b6dc6a08
zackZhang <zhangkaiamm@gmail.com>
2026-04-23 19:42:54 +0800

    Fix cache - add no-cache headers and anti-cache query param

--- api/etf.js
+++ api/etf.js
@@ -8,6 +8,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
